af50586803fdc70c93a0b532183023dfd91e1285
MarcG <marcgalaxy02@gmail.com>
2022-10-14 19:31:36 +0200

    List View Commit con Ip de la URL

--- app/src/main/java/com/example/threadscomunicacio/MainActivity.java
+++ app/src/main/java/com/example/threadscomunicacio/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void run() {
                                 text.append(res + "\n");
+                                listilla.add(res);
                                 adapter.notifyDataSetChanged();
                             }
                         });
